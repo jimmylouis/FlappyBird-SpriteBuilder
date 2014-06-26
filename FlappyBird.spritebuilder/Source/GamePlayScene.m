89f7b17c464ea0c53d252ec7bb22a399b86784c4
Jimmy Louis <jimmylouis@jimboy-mac-mini.home>
2014-06-25 21:45:14 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -17,10 +17,10 @@
 }
 
 // put new methods here
-- (void)touchBegan:(UITouch *)touch withEvent:(UIevent *)event {
-    //this will get called every time the layer touches the screen
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // this will get called every time the player touches the screen
     [character flap];
 }
-
 
 @end
